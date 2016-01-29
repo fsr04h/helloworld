87bd4500ab23d40b3ab28251af1d7259ea1a3ace
fsr04h <ionsgltos@gmail.com>
2016-01-29 12:54:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='hello-world',
     version='0.1',
-    description='simple hello world'
-    packages=['hello-world',],
-    license='Creative Commons Attribution-Noncommercial license',
-    long_description=open('README.txt').read(),
+    description='simple hello world',
+    packages=['helloworld'],
+    license='none',
+    long_description=open('README.md').read(),
 )
